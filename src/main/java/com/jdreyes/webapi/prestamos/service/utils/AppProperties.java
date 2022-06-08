e2ec0fc025df14a8abd2927d10ec6f5697691304
Josue Reyes <josuereyeschevy@gmail.com>
2022-06-07 23:09:22 -0600

    Agrega clase que contiene la informacion de la app

---
+++ src/main/java/com/jdreyes/webapi/prestamos/service/utils/AppProperties.java
@@ -0,0 +1,21 @@
+package com.jdreyes.webapi.prestamos.service.utils;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+public final class AppProperties {
+    @Value("${application.name}")
+    private String applicationName;
+
+    @Value("${build.version}")
+    private String buildVersion;
+
+    @Value("${build.timestamp}")
+    private String buildTimestamp;
+
+    @Value("${cypher.enabled}")
+    private boolean cypherEnabled;
+}
