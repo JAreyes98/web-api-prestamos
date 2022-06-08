500d686694df35004eb9c7720274fbf565b81341
Josue Reyes <josuereyeschevy@gmail.com>
2022-06-07 22:43:31 -0600

    Inicia proyecto de api para prestamos de financiera

---
+++ src/main/java/com/jdreyes/webapi/prestamos/WebApiPrestamosApplication.java
@@ -0,0 +1,13 @@
+package com.jdreyes.webapi.prestamos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApiPrestamosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApiPrestamosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jdreyes/webapi/prestamos/WebApiPrestamosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdreyes.webapi.prestamos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApiPrestamosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
