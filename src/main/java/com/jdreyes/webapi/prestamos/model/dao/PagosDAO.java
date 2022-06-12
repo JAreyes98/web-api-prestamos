7073c5c84ffe4ae4565ee5e31e8517077edeaf9f
Josue Reyes <josuereyeschevy@gmail.com>
2022-06-11 23:26:31 -0600

    Agrega implementacion y documentacion de los metodos

--- src/main/java/com/jdreyes/webapi/prestamos/model/dao/PagosDAO.java
+++ src/main/java/com/jdreyes/webapi/prestamos/model/dao/PagosDAO.java
@@ -1,11 +1,43 @@
 package com.jdreyes.webapi.prestamos.model.dao;
 
-import com.jdreyes.webapi.prestamos.model.dto.CobrosDiaDto;
+import com.jdreyes.webapi.prestamos.model.dto.CobroPrestamoDto;
 
 import java.math.BigDecimal;
+import java.util.List;
 
+/**
+ * Definición del acceso a datos de los pagos.
+ *
+ * @since 1.0, 11/06/2022
+ * @author Josue Reyes
+ * @version 1.0
+ */
 public interface PagosDAO {
-    CobrosDiaDto getCobrosDia(Integer idFuncionario);
-    boolean grabarReciboCaja(Integer idPrestamo, Integer idFuncionario);
-    boolean grabarDeposito(Integer idFuncinario, String noCuenta, BigDecimal monto);
+  /**
+   * Obtiene los cobros del dia.
+   *
+   * @param idFuncionario identificador del funcionario.
+   * @return cobros que debe realizar el funcionario durante el dia en curso.
+   */
+  List<CobroPrestamoDto> getCobrosDia(Integer idFuncionario);
+
+  /**
+   * Graba un pago relacionado a una cuota del prestamo {@code idPrestamo}.
+   *
+   * @param idPrestamo prestamos
+   * @param monto monto pagado
+   * @return bandera que indica si se grabo el resibo.
+   */
+  boolean grabarReciboCaja(Integer idPrestamo, BigDecimal monto);
+
+  /**
+   * Graba deposito bancario.
+   *
+   * @param idFuncinario indentificador del funcionario que realiza el deposito.
+   * @param noCuenta cuanta a la que se deposita.
+   * @param noMinuta no refencia/minuta
+   * @param monto monto depositado.
+   * @return bandera que indica si se grabo el deposito.
+   */
+  boolean grabarDeposito(Integer idFuncinario, String noCuenta, Integer noMinuta, BigDecimal monto);
 }
--- src/main/java/com/jdreyes/webapi/prestamos/model/dao/impl/PagosDAOImpl.java
+++ src/main/java/com/jdreyes/webapi/prestamos/model/dao/impl/PagosDAOImpl.java
@@ -2,25 +2,70 @@ package com.jdreyes.webapi.prestamos.model.dao.impl;
 
 import com.jdreyes.persistence.impl.BaseQueryExecutor;
 import com.jdreyes.webapi.prestamos.model.dao.PagosDAO;
-import com.jdreyes.webapi.prestamos.model.dto.CobrosDiaDto;
+import com.jdreyes.webapi.prestamos.model.dto.CobroPrestamoDto;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
-
+import java.util.List;
+/**
+ * Implementación del acceso a datos de los pagos.
+ *
+ * @since 1.0, 11/06/2022
+ * @author Josue Reyes
+ * @version 1.0
+ * @see com.jdreyes.persistence.BaseQueryExcecutor
+ * @see com.jdreyes.webapi.prestamos.model.dao.PagosDAO
+ */
 @Repository
 public class PagosDAOImpl extends BaseQueryExecutor implements PagosDAO {
-    @Override
-    public CobrosDiaDto getCobrosDia(Integer idFuncionario) {
-        return null;
-    }
 
-    @Override
-    public boolean grabarReciboCaja(Integer idPrestamo, Integer idFuncionario) {
-        return false;
-    }
+  /**
+   * Obtiene los cobros del dia llamando al procedimiento Sp_Cobros_del_Dia y la data obtenida se
+   * mapea hacia una entidad de tipo {@link CobroPrestamoDto}.
+   *
+   * @param idFuncionario identificador del funcionario.
+   * @return cobros que debe realizar el funcionario durante el dia en curso.
+   */
+  @Override
+  public List<CobroPrestamoDto> getCobrosDia(Integer idFuncionario) {
+    return createStoredProcedureQuery("call Sp_Cobros_del_Dia :idFuncionario", "cobrosDiaDtoMapper")
+        .setParameter("idFuncionario", idFuncionario)
+        .getResultList();
+  }
 
-    @Override
-    public boolean grabarDeposito(Integer idFuncinario, String noCuenta, BigDecimal monto) {
-        return false;
-    }
+  /**
+   * Graba un pago relacionado a una cuota del prestamo {@code idPrestamo}.
+   *
+   * @param idPrestamo prestamos
+   * @param monto monto pagado
+   * @return bandera que indica si se grabo el resibo.
+   */
+  @Override
+  public boolean grabarReciboCaja(Integer idPrestamo, BigDecimal monto) {
+    return createStoredProcedureQuery("call Sp_Graba_recibo_Caja :p_id_Prestamos, :P_mont")
+            .setParameter("p_id_Prestamos", idPrestamo)
+            .setParameter("P_mont", monto)
+            .executeUpdate()
+        > 0;
+  }
+
+  /**
+   * Graba deposito bancario.
+   *
+   * @param idFuncinario indentificador del funcionario que realiza el deposito.
+   * @param noCuenta cuanta a la que se deposita.
+   * @param noMinuta numero de referencia.
+   * @param monto monto depositado.
+   * @return bandera que indica si se grabo el deposito.
+   */
+  @Override
+  public boolean grabarDeposito(Integer idFuncinario, String noCuenta, Integer noMinuta, BigDecimal monto) {
+    return createStoredProcedureQuery("call Sp_Graba_deposito :P_Id_Gestor, :P_Id_Cuenta_Bancaria, :p_Minuta :P_Monto")
+            .setParameter("P_Id_Gestor", idFuncinario)
+            .setParameter("P_Id_Cuenta_Bancaria", noCuenta)
+            .setParameter("p_Minuta", noMinuta)
+            .setParameter("P_Monto", monto)
+            .executeUpdate()
+            > 0;
+  }
 }
