df9d5278b49b863fb2ca0687a2c4711aaa88799d
Josue Reyes <josuereyeschevy@gmail.com>
2022-06-11 23:27:36 -0600

    Agrega implementacion para llamado del detalle prestamo.

--- src/main/java/com/jdreyes/webapi/prestamos/model/dao/DetallePrestamoDAO.java
+++ src/main/java/com/jdreyes/webapi/prestamos/model/dao/DetallePrestamoDAO.java
@@ -4,5 +4,8 @@ import com.jdreyes.persistence.BaseDAO;
 import com.jdreyes.webapi.prestamos.model.entities.DetallePrestamo;
 import com.jdreyes.webapi.prestamos.model.entities.DetallePrestamoId;
 
+import java.util.List;
+
 public interface DetallePrestamoDAO extends BaseDAO<DetallePrestamo, DetallePrestamoId> {
+List<DetallePrestamo> getTablaPlagos(Integer idPrestamo);
 }
--- src/main/java/com/jdreyes/webapi/prestamos/model/dao/impl/DetallePrestamoDAOImpl.java
+++ src/main/java/com/jdreyes/webapi/prestamos/model/dao/impl/DetallePrestamoDAOImpl.java
@@ -6,6 +6,18 @@ import com.jdreyes.webapi.prestamos.model.entities.DetallePrestamo;
 import com.jdreyes.webapi.prestamos.model.entities.DetallePrestamoId;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public class DetallePrestamoDAOImpl extends BaseDAOImpl<DetallePrestamo, DetallePrestamoId> implements DetallePrestamoDAO {
+public class DetallePrestamoDAOImpl extends BaseDAOImpl<DetallePrestamo, DetallePrestamoId>
+    implements DetallePrestamoDAO {
+  @Override
+  public List<DetallePrestamo> getTablaPlagos(Integer idPrestamo) {
+    List<DetallePrestamo> list =
+        entityManager
+            .createQuery("select d from DetallePrestamo d where d.idPrestamo = :idPrestamo")
+            .setParameter("idPrestamo", idPrestamo)
+            .getResultList();
+    return list;
+  }
 }
