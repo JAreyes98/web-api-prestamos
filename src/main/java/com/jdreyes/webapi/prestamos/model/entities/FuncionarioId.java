d3c7130dde8812149131bf0462d3e76b7ca1f376
Josue Reyes <josuereyeschevy@gmail.com>
2022-06-11 18:29:31 -0600

    Agrega comentarios

--- src/main/java/com/jdreyes/webapi/prestamos/model/entities/FuncionarioId.java
+++ src/main/java/com/jdreyes/webapi/prestamos/model/entities/FuncionarioId.java
@@ -5,6 +5,14 @@ import javax.persistence.Column;
 import javax.persistence.Id;
 import java.io.Serializable;
 
+/**
+ * Entidad que representa el modelo de datos que representa la clave compuesta por el comañia, idFuncionario y sucursal.
+ * del prestamo.
+ *
+ * @author Josue Reyes
+ * @version 1.0
+ * @since 1.0, 2022-06-07
+ */
 public class FuncionarioId implements Serializable {
     private Integer companyId;
     private Integer idFuncionario;
