32b75df929c1520c1b241db0bb99051b44899128
Josue Reyes <josuereyeschevy@gmail.com>
2022-06-11 18:42:04 -0600

    Agrega comentarios

--- src/main/java/com/jdreyes/webapi/prestamos/model/dto/UserBase.java
+++ src/main/java/com/jdreyes/webapi/prestamos/model/dto/UserBase.java
@@ -3,11 +3,19 @@ package com.jdreyes.webapi.prestamos.model.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Clase base para los usuarios.
+ *
+ * @author Josue Reyes
+ * @version 1.0
+ * @since 1.0, 2022-06-7
+ */
 @Getter
 @Setter
 public abstract class UserBase {
-//    private Integer idCia;
-    private String userName;
-    private String password;
-
+  //    private Integer idCia;
+  /** Nombre de usuario. */
+  private String userName;
+  /** Contraseña. */
+  private String password;
 }
