777384683a85f5dc19de54fba9a1401e431fff7a
Josue Reyes <josuereyeschevy@gmail.com>
2022-06-11 18:28:41 -0600

    Agrega comentarios

--- src/main/java/com/jdreyes/webapi/prestamos/model/entities/DetallePrestamoId.java
+++ src/main/java/com/jdreyes/webapi/prestamos/model/entities/DetallePrestamoId.java
@@ -2,6 +2,14 @@ package com.jdreyes.webapi.prestamos.model.entities;
 
 import java.io.Serializable;
 
+/**
+ * Entidad que representa el modelo de datos que representa la clave compuesta por el id y no cuota
+ * del prestamo.
+ *
+ * @author Josue Reyes
+ * @version 1.0
+ * @since 1.0, 2022-06-07
+ */
 public class DetallePrestamoId implements Serializable {
   private static final long serialVersionUID = 1L;
   private Integer idPrestamo;
